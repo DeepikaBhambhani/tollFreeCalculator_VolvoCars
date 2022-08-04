1a15bd64823e0c83a2192373132a41e23126034c
Deepika <104832278+DeepikaBhambhani@users.noreply.github.com>
2022-08-04 14:24:45 +0200

    Update TollCalculatorService.java

--- src/main/java/com/example/tollfreecalculator/controller/TollCalculatorService.java
+++ src/main/java/com/example/tollfreecalculator/controller/TollCalculatorService.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
 public interface TollCalculatorService {
     boolean isTollFreeVehicle(Vehicle vehicle);
     boolean isTollFreeDate(LocalDate date);
